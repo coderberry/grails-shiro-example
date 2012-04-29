d6e595b23bce1169fb99b2e6920fcd1fa027b551
Eric Berry <cavneb@gmail.com>
2012-04-29 11:28:16 -0600

    added auth and home controller

--- grails-app/conf/com/example/SecurityFilters.groovy
+++ grails-app/conf/com/example/SecurityFilters.groovy
@@ -5,15 +5,68 @@ package com.example
  * via access control by convention.
  */
 class SecurityFilters {
-    def filters = {
-        all(uri: "/**") {
-            before = {
-                // Ignore direct views (e.g. the default main index page).
-                if (!controllerName) return true
 
-                // Access control by convention.
-                accessControl()
+    /**
+     * Array of controller/action combinations which will be skipped from authentication
+     * if the controller and action names match. The action value can also be '*' if it
+     * encompasses all actions within the controller.
+     */
+    static nonAuthenticatedActions = [
+            [controller: 'home', action: 'index']
+    ]
+
+    /**
+     * Array of controller/action combinations that will be authenticated against the user's
+     * role. The map also includes the roles which the controller/action pair will match
+     * against.
+     */
+    static authenticatedActions = [
+            [controller: 'home', action: 'secured', roles: ['ROLE_ADMIN', 'ROLE_USER']],
+            [controller: 'home', action: 'admin', roles: ['ROLE_ADMIN']]
+    ]
+
+    def filters = {
+
+        all(controller: '*', action: '*') {
+            before = {
+
+                // Determine if the controller/action belongs is not to be authenticated
+                def needsAuth = !nonAuthenticatedActions.find {
+                    (it.controller == controllerName) &&
+                            ((it.action == '*') || (it.action == actionName))
+                }
+
+                if (needsAuth) {
+
+                    // Get the map within the authenticated actions which pertain to the current
+                    // controller and view.
+                    def authRoles = authenticatedActions.find {
+                        (it.controller == controllerName) &&
+                                ((it.action == '*') || (it.action == actionName))
+                    }
+
+                    if (authRoles) {
+
+                        // Perform the access control for each of the roles provided in the authRoles
+                        accessControl {
+                            authRoles.roles.each { roleName ->
+                                role(roleName)
+                            }
+                        }
+                    }
+
+                    // Skip authentication if the authRoles was not found
+                    else {
+                        return true
+                    }
+                }
+
+                // Skip authentication if no auth is needed
+                else {
+                    return true
+                }
             }
         }
+
     }
 }
--- grails-app/controllers/com/example/HomeController.groovy
+++ grails-app/controllers/com/example/HomeController.groovy
@@ -2,5 +2,16 @@ package com.example
 
 class HomeController {
 
-    def index() { }
+    def index() {
+        render "This page is not secured"
+    }
+
+    def secured() {
+        render "This page requires a user to be logged in"
+    }
+
+    def admin() {
+        render "This page requires the logged in user to be an administrator"
+    }
+    
 }
